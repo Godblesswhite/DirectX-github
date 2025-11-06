463417afb6575755d947dce774c15b56c6688998
Godblesswhite <2811973868@qq.com>
2025-11-06 11:16:51 +0800

    chili 居然把WinMain给改了，之前的东西都是测试，WindowsMessageMap也是测试的，所以也不include了##

--- DirectX/WinMain.cpp
+++ DirectX/WinMain.cpp
@@ -1,9 +1,54 @@
-#include <windows.h>
 
+
+#include <windows.h>
+#include "WindowsMessageMap.h"
+#include <sstream> 
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static WindowsMessageMap mm;
+	OutputDebugStringA(mm(msg, lParam, wParam).c_str());
+
+	switch (msg)
+	{
+	case WM_CLOSE://once click exit ,msg will in this case
+		PostQuitMessage(69);//asign msg.wParam 69
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, L"Fujii Kaze");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, L"kirari");
+
+		}
+		break;
+
+	case WM_CHAR:
+	{
+		static std::string title;
+		title.push_back((char)wParam);
+		SetWindowTextA(hWnd, title.c_str());
+	}
+	break;
+	case WM_LBUTTONDOWN:
+	{
+		const POINTS pt = MAKEPOINTS(lParam);
+
+
+		std::ostringstream oss;
+		oss << "Mouse Click at: (" << pt.x << ", " << pt.y << ")";
+
+		SetWindowTextA(hWnd, oss.str().c_str());
+
+
+	}
+	break;
+	}
+
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 //DefWindowProc д╛хо
@@ -46,11 +91,19 @@ int CALLBACK WinMain(
 	// show the damn window
 	ShowWindow(hWnd, SW_SHOW);
 
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0) > 0)
+	MSG msg;//Receive mouse interaction
+	BOOL gresult;
+	while ((gresult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-	return 0;
+	if (gresult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
